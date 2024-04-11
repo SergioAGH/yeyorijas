003c870d4c4e2d04a351112d741692fdd4f106d1
SergioAGH <yeyorijas@gmail.com>
2024-04-10 21:35:24 -0600

    okk

--- script.js
+++ script.js
@@ -1,55 +1,15 @@
-const IDBReques = indexedDB.open("yeyodatos",1)
-
-IDBReques.addEventListener("upgradeneeded",()=>{
-    const db = IDBReques.result;
-    db.createObjectStore("nombres",{
-        autoIncrement: true
-    });
-})
-IDBReques.addEventListener("success",()=>{
-    console.log("todo salio correctamente");
-})
-
-IDBReques.addEventListener("error",()=>{
-    console.log("ocurrio un error");
-})
-
-const addObjetos = objeto =>{
-    const db = IDBReques.result;
-    const IDBtransaction = db.transaction("nombres","readwrite");
-    const objectStore = IDBtransaction.objectStore("nombres");
-    objectStore.add(objeto);
-    IDBtransaction.addEventListener("complete",()=>{
-        console.log("objeto agregado")
-    })
+const indexedDb = window.indexedDB;
+let db;
+const conexion = indexedDb.open("listaDeTreas",1);
+conexion.onsuccess = ()=>{
+    db = conexion.result;
+    console.log("base abierta", db);
 }
-const leerObjetos = ()=>{
-    const db = IDBReques.result;
-    const IDBtransaction = db.transaction("nombres","readonly");
-    const objectStore = IDBtransaction.objectStore("nombres");
-    const cursor = objectStore.openCursor();
-    cursor.addEventListener("success",()=>{
-        if (cursor.result){
-            console.log(cursor.result.value);
-            cursor.result.continue();
-        } else console.log("todo los datos fueron leidos")
-    })
-}
-const modificarObjeto = (key,objeto) =>{
-    const db = IDBReques.result;
-    const IDBtransaction = db.transaction("nombres","readwrite");
-    const objectStore = IDBtransaction.objectStore("nombres");
-    objectStore.put(objeto,key);
-    IDBtransaction.addEventListener("complete",()=>{
-        console.log("objeto modificado")
-    })
-}    
-const eliminarObjetos = key =>{
-    const db = IDBReques.result;
-    const IDBtransaction = db.transaction("nombres","readwrite");
-    const objectStore = IDBtransaction.objectStore("nombres");
-    objectStore.delete(key);
-    IDBtransaction.addEventListener("complete",()=>{
-        console.log("objeto eliminado")
-    })
+conexion.onupgradeneeded = (e)=>{
+    db.result;
+    console.log("baseCreada", db);
+    const coleccionObjetos = db.createObjectStore("tareas",{keyPath: "clave"});
+} 
+conexion.onerror = (error)=>{
+    console.log("error", error);
 }
